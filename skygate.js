89e4a1a637e715055920972fb06b8fa6d1ada5de
kmulewicz1 <41126247+kmulewicz1@users.noreply.github.com>
2019-11-26 19:57:28 +0100

    refactor

--- skygate.js
+++ skygate.js
@@ -1,28 +1,42 @@
 let countries = ["Poland","Germany","Spain","France"];
-let map1 = new Map();
-map1.set('poland', 'PL');
-map1.set('germany', 'DE');
-map1.set('spain', 'ES');
-map1.set('france', 'FR');
+let mapCountries = new Map();
+mapCountries.set('poland', 'PL');
+mapCountries.set('germany', 'DE');
+mapCountries.set('spain', 'ES');
+mapCountries.set('france', 'FR');
 
 document.getElementById("myInput").value=sessionStorage.getItem('key1');
-
-let divOutOfMyInput=document.getElementById("divOutOfMyInput");
 
 function checkPatternInConutries(pattern,arr)
 {
 	for(let i=0; i<arr.length;i++)
 	{
-		if (arr[i].substr(0, pattern.length).toUpperCase() == pattern.toUpperCase())
+		if (arr[i].substr(0, pattern.length).toUpperCase() === pattern.toUpperCase())
 			return true;
 		
-}//for
+}
 return false;
-}//checkPatternInConutries
+}
 
-let arrTrueFalse=[false,false,false,false,false,false,false,false,false,false,];
-function autocomplete(arg, arr) 
-{
+let arrBooleanCountries=[false,false,false,false,false,false,false,false,false,false,];
+
+
+function addCities(val, tmp,arr,divToJoin,i) {
+	if (val.length!==0&&arr[i].substr(0, val.length).toUpperCase() === val.toUpperCase()){
+
+		tmp.innerHTML=arr[i];
+		tmp.style.fontSize="17px";
+		tmp.style.backgroundColor="white";
+		tmp.style.width="64%";
+		tmp.addEventListener('click', function (event) {
+			document.getElementById("myInput").value=this.innerHTML;
+		});
+
+		divToJoin.appendChild(tmp);
+	}
+}
+
+function autocomplete(arg, arr) {
 
 	let divToJoin=document.getElementById("divToJoin");
 	arg.addEventListener("input", function(e) {
@@ -32,82 +46,60 @@ function autocomplete(arg, arr)
 	tmp.className="listOfCountry";
 	for(let i=0; i<arr.length;i++)
 	{
-		
-			if (val.length!=0&&arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()){
-				
-				tmp.innerHTML=arr[i];
-				tmp.style.fontSize="17px";
-				tmp.style.backgroundColor="white";
-				tmp.style.width="64%";
-				tmp.addEventListener('click', function (event) {
-				document.getElementById("myInput").value=this.innerHTML;
-				
-				});	//function click		
-				divToJoin.appendChild(tmp);
-				
-		}//if
-	}//for
+		addCities(val,tmp,arr,divToJoin,i);
+	}
   });
   arg.addEventListener("input", function(e) {
-	  if(arg.value.length==0)
+	  if(arg.value.length===0)
 		  deleteAutocomplete();
   });
-}//autocomplete
+}
 
-
-function deleteAutocomplete()
-{
-	
+function deleteAutocomplete() {
 	const NodeForChild=document.getElementById("divToJoin");
 	while (NodeForChild.firstChild) {
 		NodeForChild.removeChild(NodeForChild.firstChild);
-		}//while
-}//deleteAutocomplete
-
+		}
+}
 
 function deleteCities()
 {
 	const NodeForChild=document.getElementById("divPollutedCity");
 	while (!(NodeForChild.lastChild===NodeForChild.firstChild)) {
 		NodeForChild.removeChild(NodeForChild.lastChild);
-		}//while
-}//deleteCities
+		}
+}
 
 
+function polluteCities() {
 
-
-function polluteCities()
-{
-	let mySet = new Set();
 	const tmpValue=document.getElementById("myInput").value;
-	if(tmpValue.length==0){
-		alert("Please Insert name of Country!");
-}//if
-else
-	{ 	let TextIndivPollutedCity=document.getElementById("TextIndivPollutedCity");
+
+	if(tmpValue.length===0){
+		alert("Please insert name of Country!");
+}
+	else {
+
+	    let TextIndivPollutedCity=document.getElementById("TextIndivPollutedCity");
 		TextIndivPollutedCity.innerHTML="The most pollutest cities:";
 		TextIndivPollutedCity.style.fontSize="40px"
-		
 		deleteCities();
-	let apiUrl="https://api.openaq.org/v1/latest?country=codecountry&order_by=measurements[0].value&sort=desc&parameter=pm25&&limit=50";
-//	let apiUrl="https://api.openaq.org/v1/measurements";
-	apiUrl= apiUrl.replace("codecountry", map1.get(tmpValue.toLowerCase()));
-	const divPollutedCity=document.getElementById("divPollutedCity");
-	let request = new XMLHttpRequest();
-	request.open('GET', apiUrl, true);
-	request.send();
-	request.onload=function (){
+        let apiUrl="https://api.openaq.org/v1/latest?country=codecountry&order_by=measurements[0].value&sort=desc&parameter=pm25&&limit=50";
+        apiUrl= apiUrl.replace("codecountry", mapCountries.get(tmpValue.toLowerCase()));
+        const divPollutedCity=document.getElementById("divPollutedCity");
+        let request = new XMLHttpRequest();
+        request.open('GET', apiUrl, true);
+        request.send();
+        request.onload=function (){
 		
 		let data = JSON.parse(this.response);
+		let myArr;
 		
-		  if (request.status == 200)
+		  if (request.status === 200)
 		  {
-			   for(let i=0; i<50; i++)
-			   {
-				   mySet.add(data.results[i].city);
+			   for(let i=0; i<10; i++) {
+				   myArr[i]=data.results[i].city;
 			   }
-			   let getEntriesArry = mySet.entries();
-				let myArr = Array.from(mySet);
 			  
 			  for(let i=0; i<10; i++)
 			  {
@@ -125,97 +117,75 @@ else
 				 });
 				 divPollutedCity.appendChild(tmp);
 			  }
-		  }//if
-		  
-	}//function
-	}
-}//polluteCities
-
-
-function Wikidescription(arg)
-{	
-
-	 if(!arrTrueFalse[arg.id])
-			  {
-	let apiUrl="https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&formatversion=2&exintro=&titles=cityname";
-	
-	let cityname=arg.innerHTML;	
-	apiUrl= apiUrl.replace("cityname", cityname);	
-	let request = new XMLHttpRequest();	
-	request.open('GET', apiUrl, true);
-	request.send();
-	request.onload=function (){
-		let data = JSON.parse(this.response);
-	
-		  if (request.status == 200)
-		  {
-			 
-				  arrTrueFalse[arg.id]=true;
-
-			  let tmp=document.createElement('DIV');
-			  if(data.query.pages[0].extract)
-			  {
-				  
-				tmp.innerHTML=data.query.pages[0].extract;
-				
-			  }
-			else
-			{
-				tmp.innerHTML="I'm sorry. In our databse this city doesn't exist";
-			}
-				if(!(arg.lastChild===arg.firstChild)) 
-					arg.removeChild(arg.lastChild);
-				
-				 arg.appendChild(tmp);
-				
-				
-		  
-		
-		   
-		  }//request==200	
-		  
-	}
-	}//argTrueFalse	
-		    else
-		  {
-			 
-			  arrTrueFalse[arg.id]=false;
-			  arg.removeChild(arg.lastChild);
 		  }
+	}
+	}
+}
+
+
+function Wikidescription(arg) {
+
+	 if(!arrBooleanCountries[arg.id]) {
+	        let apiUrl="https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&formatversion=2&exintro=&titles=cityname";
+	        let cityname=arg.innerHTML;
+            apiUrl= apiUrl.replace("cityname", cityname);
+            let request = new XMLHttpRequest();
+            request.open('GET', apiUrl, true);
+            request.send();
+            request.onload=function (){
+            let data = JSON.parse(this.response);
+
+              if (request.status === 200) {
+                      arrBooleanCountries[arg.id]=true;
+                      let tmp=document.createElement('DIV');
+                      if(data.query.pages[0].extract) {
+                          tmp.innerHTML=data.query.pages[0].extract;
+                  }
+                else {
+                    tmp.innerHTML="I'm sorry. In our databse this city doesn't exist";
+                        }
+                    if(!(arg.lastChild===arg.firstChild)) {
+                        arg.removeChild(arg.lastChild);
+                    }
+                     arg.appendChild(tmp);
+                }}}
+            else {
+              arrBooleanCountries[arg.id]=false;
+              arg.removeChild(arg.lastChild);
+             }
 }
 
 
 
-function validate(evt) {	
-	let key = evt.keyCode;
-	key = String.fromCharCode(key);  
-	const myInputValue=document.getElementById("myInput").value;
-	
-	if( !checkPatternInConutries(myInputValue+key,countries)) {
+function validate(evt) {
+    let key = evt.keyCode;
+    key = String.fromCharCode(key);
+    const myInputValue=document.getElementById("myInput").value;
+
+    if(!checkPatternInConutries(myInputValue+key,countries)) {
     evt.returnValue = false;
-  }//if
-}//validate 
-
-
-function saveIt()
-{
-	sessionStorage.clear();
-	sessionStorage.setItem('key1',document.getElementById("myInput").value)
+  }
 }
+
+
+function saveIt() {
+    sessionStorage.clear();
+    sessionStorage.setItem('key1',document.getElementById("myInput").value)
+}
+
+
 autocomplete(document.getElementById("myInput"), countries);
 
 
 $(document).ready(function() {
 
     $("#btnCity").click(function() {
-		
-		if(document.getElementById("myInput").value){
-     
+
+        if(document.getElementById("myInput").value){
         $("#divOutOfMyInput").animate(
-		{
-			top:"1px"
-			
-		},1000);
-		}//if      
+        {
+            top:"1px"
+        },1000);
+        }
     });
-}); 
+});
